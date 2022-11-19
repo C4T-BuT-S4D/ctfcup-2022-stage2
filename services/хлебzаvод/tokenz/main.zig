3782d8d3173f1b17dc079174f730e76d7136b226
renbou <artemmikheev1311@gmail.com>
2022-11-19 21:00:28 +0300

    хлебzаvод: tokenz: signing implemented

--- services/хлебzаvод/tokenz/main.zig
+++ services/хлебzаvод/tokenz/main.zig
@@ -6,12 +6,14 @@ const graceful = @import("./src/graceful.zig");
 // pub const io_mode = .evented;
 
 pub fn main() !void {
-    // var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    // var allocator = gpa.allocator();
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    var allocator = gpa.allocator();
 
     var svc = try auth.Service.init("auth-state");
 
-    std.log.debug("{s} {}", .{ svc.secretKey, svc.nonce.value });
+    std.log.debug("{s}", .{svc.secretKey});
+
+    std.log.debug("{s}", .{try svc.sign(allocator, "aboba")});
 
     // var server = try web.Server.init(gpa.allocator(), "0.0.0.0:80");
 
--- services/хлебzаvод/tokenz/src/auth.zig
+++ services/хлебzаvод/tokenz/src/auth.zig
@@ -2,14 +2,13 @@ const std = @import("std");
 const aes = std.crypto.aead.aes_gcm.Aes256Gcm;
 
 pub const Service = struct {
-    random: DevUrandom,
+    const tokenKeyLength = 4;
+
     path: []const u8,
     secretKey: [aes.key_length]u8 = std.mem.zeroes([aes.key_length]u8),
-    nonce: std.atomic.Atomic(u64) = .{ .value = 0 },
 
     pub fn init(path: []const u8) !Service {
         var service = Service{
-            .random = try DevUrandom.init(),
             .path = path,
         };
 
@@ -29,14 +28,10 @@ pub const Service = struct {
 
         const stream = file.reader();
         try stream.readNoEof(self.secretKey[0..]);
-
-        var nonceBytes: [8]u8 = undefined;
-        try stream.readNoEof(nonceBytes[0..]);
-        self.nonce.store(std.mem.readIntSliceNative(u64, nonceBytes[0..]), .SeqCst);
     }
 
     fn create(self: *Service) !void {
-        try self.random.read(self.secretKey[0..]);
+        std.crypto.random.bytes(self.secretKey[0..]);
         try self.save();
     }
 
@@ -44,40 +39,63 @@ pub const Service = struct {
         var file = try std.fs.cwd().createFile(self.path, .{});
         defer file.close();
 
-        var state: [aes.key_length + 8]u8 = undefined;
-        std.mem.copy(u8, state[0..], self.secretKey[0..]);
-        std.mem.writeIntSliceNative(u64, state[self.secretKey.len..], self.nonce.load(.SeqCst));
-
-        try file.writeAll(state[0..]);
-    }
-};
-
-const DevUrandom = struct {
-    fd: std.os.fd_t,
-    file: std.fs.File,
-    br: std.io.BufferedReader(4096, std.fs.File.Reader),
-    mu: std.Thread.Mutex,
-
-    fn init() !DevUrandom {
-        const fd = try std.os.openZ("/dev/urandom", std.os.system.O.RDONLY | std.os.system.O.CLOEXEC, 0);
-        const file = std.fs.File{
-            .handle = fd,
-            .capable_io_mode = .blocking,
-            .intended_io_mode = .blocking,
-        };
-
-        return DevUrandom{
-            .fd = fd,
-            .file = file,
-            .br = std.io.bufferedReader(file.reader()),
-            .mu = .{},
-        };
+        try file.writeAll(self.secretKey[0..]);
     }
 
-    fn read(self: *DevUrandom, buf: []u8) !void {
-        self.mu.lock();
-        defer self.mu.unlock();
+    pub fn sign(self: *Service, allocator: std.mem.Allocator, data: []const u8) ![]const u8 {
+        const tokenKey = try self.generateTokenKey();
+        const encryptedData = try self.encryptData(allocator, tokenKey, data);
 
-        try self.br.reader().readNoEof(buf);
+        var nonce = try self.generateNonce();
+        var encryptedTokenKey: [Service.tokenKeyLength]u8 = undefined;
+        var tag: [aes.tag_length]u8 = undefined;
+
+        aes.encrypt(encryptedTokenKey[0..], &tag, tokenKey[0..], encryptedData, nonce, self.secretKey);
+
+        const encoder = std.base64.url_safe_no_pad.Encoder;
+        const parts = [_][]const u8{ encryptedData, encryptedTokenKey[0..], nonce[0..], tag[0..] };
+        var size: usize = 0;
+        for (parts) |part, index| {
+            size += encoder.calcSize(part.len);
+            if (index < parts.len - 1) {
+                size += 1;
+            }
+        }
+
+        const token = try allocator.alloc(u8, size);
+        var position: usize = 0;
+        for (parts) |part, index| {
+            position += encoder.encode(token[position..], part).len;
+            if (index < parts.len - 1) {
+                token[position] = '.';
+                position += 1;
+            }
+        }
+
+        return token;
+    }
+
+    fn generateTokenKey(_: *Service) ![Service.tokenKeyLength]u8 {
+        var key: [Service.tokenKeyLength]u8 = undefined;
+        std.crypto.random.bytes(key[0..]);
+
+        return key;
+    }
+
+    fn generateNonce(_: *Service) ![aes.nonce_length]u8 {
+        var nonce: [aes.nonce_length]u8 = undefined;
+        std.crypto.random.bytes(nonce[0..]);
+
+        return nonce;
+    }
+
+    fn encryptData(_: *Service, allocator: std.mem.Allocator, tokenKey: [Service.tokenKeyLength]u8, data: []const u8) ![]const u8 {
+        const encrypted = try allocator.alloc(u8, data.len);
+        std.mem.copy(u8, encrypted, data);
+
+        for (data) |_, index| {
+            encrypted[index] ^= tokenKey[index % tokenKey.len];
+        }
+        return encrypted;
     }
 };
