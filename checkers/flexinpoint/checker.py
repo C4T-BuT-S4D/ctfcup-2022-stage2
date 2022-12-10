1d169a0e9531be2d50a793ccd22a0e28430505d9
Roman Nikitin <nikrom2012@me.com>
2022-12-10 17:22:34 +0300

    [hotfixes] Fix flexinpoint checker termination, add great_mettender unintended writeup

--- checkers/flexinpoint/checker.py
+++ checkers/flexinpoint/checker.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import os
+import signal
 import subprocess
 import sys
 from pathlib import Path
@@ -7,5 +7,15 @@ from pathlib import Path
 BASE_DIR = Path(__file__).absolute().parent
 BIN_PATH = BASE_DIR / 'flexinpoint_checker'
 
-p = subprocess.run([str(BIN_PATH), *sys.argv[1:]], shell=False, check=False)
-sys.exit(p.returncode)
+p = subprocess.Popen([str(BIN_PATH), *sys.argv[1:]], shell=False)
+
+
+def exit_handler(*_args):
+    print('Killing subprocess', file=sys.stderr)
+    p.kill()
+
+
+signal.signal(signal.SIGINT, exit_handler)
+signal.signal(signal.SIGTERM, exit_handler)
+
+sys.exit(p.wait())
